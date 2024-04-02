1ae0f633a01ca33fc209bce68129e3b0f1827d8e
Meowwwza <sorawit.3p@gmail.com>
2024-04-03 01:21:01 +0700

    ืnamtor.php

--- InsertCalendarSystem/views/layout.php
+++ InsertCalendarSystem/views/layout.php
@@ -126,6 +126,26 @@
                     </a>
                 </li>
 
+                <li>
+                    <a class="menu-icon" href="./name.php" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-clock" viewBox="0 0 16 16">
+                            <i class="icon-note menu-icon">ของเนม</i>
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                        </svg><span class="nav-text menu-icon">&nbsp;</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="menu-icon" href="./namtor.php" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-clock" viewBox="0 0 16 16">
+                            <i class="icon-note menu-icon">ของน้่ำตาล</i>
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                        </svg><span class="nav-text menu-icon">&nbsp;</span>
+                    </a>
+                </li>
+
 
             </ul>
         </div>
